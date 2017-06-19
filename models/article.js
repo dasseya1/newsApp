09edaa2148a03ef30a8c1bf8a1b8a70a6a26099c
Damien Asseya <damienasseya@Users-MacBook-Pro.local>
2017-06-18 22:49:46 -0400

    Created package.json and model for articles

---
+++ models/article.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+
+    title: { type: String, unique: true },
+    link: { type: String, unique: true },
+    image: { type: String, unique: true },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+var Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
